4fd9523421d8e08eb335c31fa24417d3db2b2efd
rodrigo <carnevale.lemobs@gmail.com>
2020-12-01 11:12:14 -0300

    commit inicial

---
+++ users/users.router.ts
@@ -0,0 +1,63 @@
+import {Router} from '../common/router'
+import * as restify from 'restify'
+import {User} from './users.model'
+
+class UsersRouter extends Router {
+  applyRoutes(application: restify.Server){
+
+    application.get('/users', (req, resp, next)=>{
+      User.find().then(users=>{
+        resp.json(users)
+        return next()
+      })
+    })
+
+    application.get('/users/:id', (req, resp, next)=>{
+      User.findById(req.params.id).then(user=>{
+        user ? resp.json(user) : resp.send(404);
+        return next();
+      })
+    })
+
+    application.post('/users', (req, resp, next)=>{
+      let user = new User(req.body)
+      user.save().then(user=>{
+        user.password = undefined
+        resp.json(user)
+        return next()
+      })
+    })
+
+    application.put('/users/:id', (req, resp, next) => {
+      // const options = {overwrite:true}
+      (<any>User).update({_id:req.params.id}, req.body, {overwrite:true}).exec().then(result => {  
+        if(result.n){
+            return User.findById(req.params.id)
+          } else {
+            resp.send(404)
+          }
+      }).then(user =>{
+        resp.json(user);
+        return next();
+      })
+    })
+
+    application.patch('/users/:id', (req,resp, next) => {
+      const opt = {new:true}
+      User.findByIdAndUpdate(req.params.id, req.body, opt).then(user => {
+        user ? resp.json(user) : resp.send(404);
+        return next();
+      })
+    })
+
+    application.del('/users/:id', (req, resp, next) => {
+      User.remove({_id:req.params.id}).exec().then((cmdResult: any) => {
+        cmdResult.result.n ? resp.send(204) : resp.send(404);
+        return next();
+      })
+    })
+
+  }
+}
+
+export const usersRouter = new UsersRouter()
